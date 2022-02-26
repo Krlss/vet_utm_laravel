10b6312accee71ac61e0905f0251faba73061665
Carlos Pico <cpico6375@gmail.com>
2022-02-25 21:35:56 -0500

    wtf2

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/ 
+*/
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard.index');
 })->name('dashboard');
+
+Route::get('/', function () {
+    return view('dashboard.index');
+});
